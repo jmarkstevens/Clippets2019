a730768f8c067c0a975a1bb2067397edf1f8b9ef
J. Mark Stevens <github@jmarkstevens.com>
2019-05-04 08:56:24 -0700

    tree-view

--- src/components/common/TreeView/tree-view.jsx
+++ src/components/common/TreeView/tree-view.jsx
@@ -1,14 +1,15 @@
 import classNames from 'classnames'
-import PropTypes from 'prop-types'
 import React from 'react'
 import lodashGet from 'lodash/get'
 
-const TreeRootSty = { lineHeight: '21px' }
+const TreeRootSty = { lineHeight: '120%' }
 const liSty = { listStyleType: 'none' }
-const ulSty = { WebkitPaddingStart: '16px' }
-const ulStyle = { WebkitPaddingStart: '16px' }
+const ulSty = { height: 'inherit', WebkitPaddingStart: '16px' }
+const ulStyle = { height: 'inherit', WebkitPaddingStart: '16px' }
 const iconStyBase = {
-  background: "url('./img/sun.ico') 0/16px no-repeat"
+  background: "url('./img/sun.ico') 0/16px no-repeat",
+  marginRight: '10px',
+  width: '16px'
 }
 
 const defaultColors = {
@@ -18,124 +19,112 @@ const defaultColors = {
   endnodeSelected: '#b58900'
 }
 
-class TreeViewNode extends React.Component {
-  static propTypes = {
-    node: PropTypes.shape({}).isRequired,
-    titleClick: PropTypes.func.isRequired
-  }
-
-  state = { visible: false }
-
-  iconHandler = () => {
-    const { node, titleClick } = this.props
-    const { visible } = this.state
-    if (node.children && node.children.length > 0) {
-      this.setState({ visible: !visible })
-    } else {
-      titleClick(node)
-    }
-  }
-
-  clickHandler = () => {
-    const { node, titleClick } = this.props
+const TreeViewNode = props => {
+  const { customColors, iconClick, node, options, titleClick } = props
+  const clickHandler = () => {
     titleClick(node)
   }
-
-  render() {
-    const { customColors, node, options, titleClick } = this.props
-    const { visible } = this.state
-    const titleSty = { margin: '1px 0' }
-    let childNodes
-    let hasChildren = false
+  const iconHandler = () => {
     if (node.children && node.children.length > 0) {
-      childNodes = node.children.map(child => (
-        <li key={child.filename} style={liSty}>
-          <TreeViewNode
-            customColors={customColors}
-            node={child}
-            options={options}
-            titleClick={titleClick}
-          />
-        </li>
-      ))
-      hasChildren = true
-    }
-    if (hasChildren) {
-      titleSty.color = node.selected
-        ? customColors.parentSelected
-        : customColors.parent
+      iconClick(node)
     } else {
-      titleSty.color = node.selected
-        ? customColors.endnodeSelected
-        : customColors.endnode
+      clickHandler()
     }
-
-    let branch = null
-    if (visible) {
-      branch = (
-        <ul id="ulStyle" key={node.filename} style={ulStyle}>
-          {childNodes}
-        </ul>
-      )
-    }
-
-    const TreeNodePClass = classNames(
-      'tree-node-p',
-      {
-        'tree-node-p-can-toggle': hasChildren
-      },
-      {
-        'tree-node-p-no-toggle': !hasChildren
-      }
-    )
-    const iconSty = Object.assign({}, iconStyBase)
-    const iconType = lodashGet(this.props, options.typeName)
-    if (iconType === options.icon.sun)
-      iconSty.background = "url('./img/sun.ico') 0/16px no-repeat"
-    else if (iconType === options.icon.leaf)
-      iconSty.background = "url('./img/leaf.ico') 0/16px no-repeat"
-    else if (iconType === options.icon.snow)
-      iconSty.background = "url('./img/snow.ico') 0/16px no-repeat"
-    // else iconSty.background = "url('./img/sun.ico') 0/16px no-repeat";
-
-    const titleName = node.title
-
-    return (
-      <div id="TreeNode">
-        <div className={TreeNodePClass}>
-          <div
-            className="tree-node-icon"
-            onClick={this.iconHandler}
-            onKeyPress={this.iconHandler}
-            role="button"
-            style={iconSty}
-            tabIndex={0}
-          >
-            &nbsp;
-          </div>
-          <div
-            id="titleSty"
-            onClick={this.clickHandler}
-            onKeyPress={this.clickHandler}
-            role="button"
-            style={titleSty}
-            tabIndex={0}
-          >
-            {titleName}
-          </div>
-        </div>
-        {branch}
-      </div>
-    )
   }
+  const titleColors = customColors || defaultColors
+  const titleSty = { marginTop: '2px' }
+  let childNodes
+  let hasChildren = false
+
+  if (node.children && node.children.length > 0) {
+    childNodes = node.children.map(child => (
+      <li key={child.nodeid} style={liSty}>
+        <TreeViewNode
+          customColors={customColors}
+          iconClick={iconClick}
+          node={child}
+          options={options}
+          titleClick={titleClick}
+        />
+      </li>
+    ))
+    hasChildren = true
+  }
+  if (hasChildren) {
+    titleSty.color = node.selected
+      ? titleColors.parentSelected
+      : titleColors.parent
+  } else {
+    titleSty.color = node.selected
+      ? titleColors.endnodeSelected
+      : titleColors.endnode
+  }
+
+  let isClosed = true
+  if (node.closed !== null) isClosed = node.closed
+
+  const branch = isClosed ? null : (
+    <ul id="ulStyle" key={node.nodeid} style={ulStyle}>
+      {childNodes}
+    </ul>
+  )
+
+  const TreeNodePClass = classNames(
+    'tree-node-p',
+    {
+      'tree-node-p-can-toggle': hasChildren
+    },
+    {
+      'tree-node-p-no-toggle': !hasChildren
+    }
+  )
+
+  const iconSty = Object.assign({}, iconStyBase)
+  const iconType = lodashGet(props, options.typeName)
+  if (iconType === options.icon.sun)
+    iconSty.background = "url('./img/sun.ico') 0/16px no-repeat"
+  else if (iconType === options.icon.leaf)
+    iconSty.background = "url('./img/leaf.ico') 0/16px no-repeat"
+  else if (iconType === options.icon.snow)
+    iconSty.background = "url('./img/snow.ico') 0/16px no-repeat"
+  // else iconSty.background = "url('./img/sun.ico') 0/16px no-repeat";
+
+  return (
+    <div id="TreeNode">
+      <div className={TreeNodePClass}>
+        <div
+          id="iconSty"
+          onClick={iconHandler}
+          onKeyPress={iconHandler}
+          role="button"
+          style={iconSty}
+          tabIndex={0}
+        >
+          &nbsp;
+        </div>
+        <div
+          id="titleSty"
+          onClick={clickHandler}
+          onKeyPress={clickHandler}
+          role="button"
+          style={titleSty}
+          tabIndex={0}
+        >
+          {node.title}
+        </div>
+      </div>
+      {branch}
+    </div>
+  )
 }
 
 const TreeView = props => {
-  const { data, options, titleClick, customColors } = props
+  const { data, options, iconClick, titleClick, customColors } = props
   const childNodes = data.map(child => (
-    <li key={child.title} style={liSty}>
+    <li key={child.nodeid} style={liSty}>
       <TreeViewNode
         customColors={customColors}
+        iconClick={iconClick}
         node={child}
         options={options}
         titleClick={titleClick}
@@ -149,17 +138,6 @@ const TreeView = props => {
       </ul>
     </div>
   )
-}
-
-TreeView.defaultProps = {
-  customColors: defaultColors
-}
-
-TreeView.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  options: PropTypes.shape({}).isRequired,
-  titleClick: PropTypes.func.isRequired,
-  customColors: PropTypes.shape({})
 }
 
 export default TreeView
